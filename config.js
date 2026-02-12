8d1510b79df66503135e025d50da46220bc1dcda
Abhinav S <abhinavskumar33@gmail.com>
2026-02-12 16:53:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const CONFIG = {
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        bears: ['🐱', '😽']                       // Cute bear emojis
     },
 
     // Questions and answers
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I LOVE YOU NGAAAAAAAAAAAAAAAAAAA"           // Secret hover message
+            secretAnswer: "I LOVE YOU NGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
